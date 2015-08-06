5bd5da1901cbaa8c9fd40bb09bf3c6482a57fae3
Luis Mariano Arobes <lmag94@gmail.com>
2015-08-06 18:10:37 -0500

    Button camera

--- bondzuios/VideoViewController.swift
+++ bondzuios/VideoViewController.swift
@@ -22,21 +22,27 @@ class VideoViewController: UIViewController {
         
         url = NSURL(string: "http://hls.live.metacdn.com/hls-live/2050C7/bednxurcj/prairiedogs2_/prairiedogs2_,576,192,.m3u8");
         
-        let cameraButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Camera, target: self, action: "cameraButtonClicked:")
-        
-        self.moviePlayerController.navigationController?.navigationItem.rightBarButtonItem = cameraButton;
-        
         self.moviePlayerController = MPMoviePlayerViewController(contentURL: url);
         self.moviePlayerController.moviePlayer.fullscreen = true;
         self.moviePlayerController.moviePlayer.controlStyle = MPMovieControlStyle.Fullscreen;
+        
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Camera, target: self, action: "cameraButtonClicked:");
+        
+        let button = UIButton(frame: CGRectMake(100, 100, 50, 50));
+        button.setTitle("Más", forState: UIControlState.Normal);
+        button.addTarget(self, action: "cameraButtonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.moviePlayerController.moviePlayer.view.addSubview(button);
+        
         self.moviePlayerController.moviePlayer.prepareToPlay();
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "doneButtonClick:", name: MPMoviePlayerPlaybackDidFinishNotification, object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
-        self.presentMoviePlayerViewControllerAnimated(self.moviePlayerController)
+        self.presentMoviePlayerViewControllerAnimated(self.moviePlayerController)        
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -47,9 +53,9 @@ class VideoViewController: UIViewController {
         println("Termino video");
     }
     
-    func cameraButtonClicked(sender: UIButton)
+    func cameraButtonClicked(sender: AnyObject)
     {
-        
+        println("Camera clicked");
     }
 }
 
