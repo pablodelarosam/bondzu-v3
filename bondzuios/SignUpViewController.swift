c93be7478b837045a941a4aeebeaecbb1e08c776
Ricardo Lopez Focil <rikyfocil@Ricardos-MacBook-Pro-3.local>
2015-09-16 17:56:09 -0500

    Todo en su lugar

--- bondzuios/SignUpViewController.swift
+++ bondzuios/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignUpViewController.swift
+//  LoginViewController.swift
 //  bondzuios
 //
 //  Created by Luis Mariano Arobes on 10/08/15.
@@ -7,26 +7,319 @@
 //
 
 import UIKit
+import Parse
+import FBSDKCoreKit
 
-class SignUpViewController: UIViewController {
+import MobileCoreServices
 
+
+class SignUpViewController : UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var mail: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var name: UITextField!
+
+    @IBOutlet weak var join: UIButton!
+    @IBOutlet weak var joinfb: UIButton!
+    
+    @IBOutlet weak var profile: UIImageView!
+    var hasImage = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.barStyle = .Black
+        self.navigationController?.navigationBar.barTintColor = Constantes.COLOR_NARANJA_NAVBAR
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        
+        pass.secureTextEntry = true
+        join.layer.borderWidth = 2
+        join.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        join.layer.cornerRadius = 10
+        joinfb.layer.cornerRadius = 10
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "dissmissKeyboards"))
+        profile.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "changeIcon"))
+        profile.userInteractionEnabled = true
+        
+        profile.layer.cornerRadius = 75/2
+        
+        if let user = PFUser.currentUser(){
+            performSegueWithIdentifier("loginSegue", sender: user)
+        }
+        
     }
-    
+
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.Portrait
-    }       
+    }
+    
+    @IBAction func login(sender: UIButton)
+    {
+        /*let vc : UITabBarController = self.storyboard!.instantiateViewControllerWithIdentifier("Tabs") as! UITabBarController
+        self.presentViewController(vc, animated: true, completion: nil);*/
+        
+        if let _ =  PFUser.logInWithUsername("demouser@demo.com", password: "demo_user"){
+            self.performSegueWithIdentifier("loginSegue", sender: self)
+        }
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func dissmissKeyboards(){
+        name.resignFirstResponder()
+        pass.resignFirstResponder()
+        mail.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func changeIcon(){
+        let controller = UIAlertController(title: "Attach image", message: "Select an image to attach to your profile", preferredStyle: .ActionSheet)
+        
+        controller.addAction(UIAlertAction(title: "Take picture", style: .Default, handler: {
+            a in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+                let controller = UIImagePickerController()
+                controller.sourceType = UIImagePickerControllerSourceType.Camera
+                controller.mediaTypes = [kUTTypeImage as String]
+                controller.allowsEditing = true
+                controller.delegate = self
+                self.presentViewController(controller, animated: true, completion: nil)
+            }
+        }))
+        controller.addAction(UIAlertAction(title: "Select from library", style: .Default, handler: {
+            a in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+                let controller = UIImagePickerController()
+                controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+                controller.mediaTypes = [kUTTypeImage as String]
+                controller.allowsEditing = true
+                controller.delegate = self
+                self.presentViewController(controller, animated: true, completion: nil)
+            }
+        }))
+        if(hasImage){
+            controller.addAction(UIAlertAction(title: "Delete image", style: .Destructive, handler: {
+                a in
+                self.profile.image = UIImage(named: "profile_pic")
+                self.hasImage = false
+            }))
+        }
+        
+        controller.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            a in
+        }))
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        let originalmage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        if let image = editedImage{
+            profile.image = image
+            hasImage = true
+        }
+        else if let image = originalmage{
+            profile.image = image
+            hasImage = true
+        }
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func register(){
+        guard name.text?.characters.count != 0 else{
+            let alert = UIAlertController(title: "Empty name", message: "Your name should not be empty", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {_ in self.name.becomeFirstResponder()}))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        guard mail.text?.characters.count != 0 else{
+            let alert = UIAlertController(title: "Empty mail", message: "Your mail should not be empty", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {_ in self.mail.becomeFirstResponder()}))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        guard (mail.text?.isValidEmail() == true) else{
+            let alert = UIAlertController(title: "Invalid mail", message: "Please insert a valid mail", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {_ in self.mail.becomeFirstResponder()}))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        guard pass.text?.characters.count >= 5 else{
+            let alert = UIAlertController(title: "Empty password", message: "Your password should contain at least 5 characters", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {_ in self.pass.becomeFirstResponder()}))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let query = PFQuery(className: "User")
+        query.whereKey("username", equalTo: (self.mail.text?.lowercaseString)!)
+        query.countObjectsInBackgroundWithBlock {
+            (v, e) -> Void in
+            guard e == nil else{
+                let a = UIAlertController(title: "Error", message: "Unable to create user. Please try again later", preferredStyle: .Alert)
+                a.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                return
+            }
+            guard v == 0 else{
+                let a = UIAlertController(title: "Error", message: "The email is already registered", preferredStyle: .Alert)
+                a.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                return
+            }
+        }
+        
+        
+        func registerFinal(){
+            let user = PFUser()
+            user.username = self.mail.text?.lowercaseString
+            user.password = self.pass.text
+            user.email = self.mail.text?.lowercaseString
+            user["name"] = self.name.text
+            
+            if hasImage{
+                user["photoFile"] = PFFile(data: UIImagePNGRepresentation(profile.image!)!)
+            }
+            user.signUpInBackgroundWithBlock(){
+                (creado, e) -> Void in
+                guard e == nil && creado else{
+                    let a = UIAlertController(title: "Error", message: "Unable to create user. Please try again later", preferredStyle: .Alert)
+                    a.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                    return
+                }
+                
+                PFUser.logInWithUsernameInBackground(self.mail
+                    .text!, password: self.pass.text!, block:
+                    { (user, error) -> Void in
+                        guard error == nil else{
+                            let a = UIAlertController(title: "Error", message: "Your user was created but we where unable to log in. Please enter your data in \"login here\"", preferredStyle: .Alert)
+                            a.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                            return
+                        }
+                        
+                        self.performSegueWithIdentifier("loginSegue", sender: self);
+                    }
+                )
+            }
+           
+        }
+
+
+        
+        
+        
+        if !hasImage{
+            let alert = UIAlertController(title: "Empty profile image", message: "You can add a profile picture. Would you like to do it?", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Sure!", style: .Default, handler:{
+                _ in
+                self.changeIcon()
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .Default, handler:{
+                _ in
+                registerFinal()
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        else{
+            registerFinal()
+        }
+        
+    }
+    
+    @IBAction func registerFacebook(sender: AnyObject) {
+        
+        
+        func process(user : PFUser?, error : NSError?){
+            if error != nil{
+                print(error)
+                let a  = UIAlertController(title: "Error", message: "Unable to sign up", preferredStyle: UIAlertControllerStyle.Alert)
+                a.addAction(UIAlertAction(title: "ok", style: .Default, handler: nil))
+                dispatch_async(dispatch_get_main_queue()){
+                    self.presentViewController(a, animated: true, completion: nil)
+                }
+            }
+            else if user!.isNew{
+                FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id,name,email,picture.width(100).height(100)"]).startWithCompletionHandler({
+                    (connection, dic, error) -> Void in
+                    if let dictionary = dic as? Dictionary<String, AnyObject>{
+                        let user = PFUser.currentUser()!
+                        user["name"] = dictionary["name"] as! String
+                        user.password = "\(random())"
+                        user["email"] = dictionary["email"] as! String
+                        user["photo"] = ((dictionary["picture"] as! Dictionary<String,AnyObject>)["data"]  as! Dictionary<String,AnyObject>)["url"] as! String
+                        user.saveInBackgroundWithBlock({
+                            (saved, error) -> Void in
+                            if error != nil{
+                                print(error)
+                                user.deleteInBackgroundWithBlock({ (del, error) -> Void in
+                                    print("Usuario eliminado")
+                                })
+                            }
+                            else{
+                                dispatch_async(dispatch_get_main_queue()){
+                                    self.performSegueWithIdentifier("loginSegue", sender: PFUser.currentUser()!)
+                                }
+                            }
+                            
+                        })
+                    }
+                })
+                print("usuario nuevo \(PFUser.currentUser()?.objectId)")
+            }
+            else{
+                dispatch_async(dispatch_get_main_queue()){
+                    self.performSegueWithIdentifier("loginSegue", sender: PFUser.currentUser()!)
+                }
+            }
+        }
+
+        
+    
+        let fbPermission = ["user_about_me","email"]
+        let login = FBSDKLoginManager()
+        login.loginBehavior = .Native
+    
+        if let at = FBSDKAccessToken.currentAccessToken(){
+            PFFacebookUtils.logInInBackgroundWithAccessToken(at, block: process)
+            return
+        }
+        
+        login.logInWithReadPermissions(fbPermission, fromViewController: self){
+            (result, error) -> Void in
+            if error != nil{
+                print(error)
+            }
+            else if result.isCancelled{
+                print("Cancelado")
+            }
+            else{
+                PFFacebookUtils.logInInBackgroundWithAccessToken(FBSDKAccessToken.currentAccessToken(), block: process)
+            }
+        }
+        
+        
+    }
     /*
-    // MARK: - Navigation
+    //MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
